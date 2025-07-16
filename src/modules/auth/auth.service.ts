031bfc88c7a725972010cbeac95c6ab4a36153b9
Mohammad Hosseini <mimhe1381@gmail.com>
2025-07-16 19:38:29 +0330

    Complete send-otp service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { SendOtpDto } from './dto/send-otp.dto';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+import { Keyv } from '@keyv/redis';
 
 @Injectable()
 export class AuthService {
-  async sendOtp({ phoneNumber }: SendOtpDto): Promise<true | never> {
+  private cacheManager: Keyv;
+
+  constructor(
+    @Inject(CACHE_MANAGER) cacheManager: Cache
+  ) {
+    this.cacheManager = cacheManager.stores[1];
+  }
+
+  async sendOtp({ phoneNumber }: SendOtpDto): Promise<boolean | never> {
+    const otp = {
+      code: Math.floor(Math.random() * 100_000),
+      expiresIn: Date.now() + 120_000
+    };
+    const result = await this.cacheManager.set(`user-${phoneNumber}`, { otp }, 180_000);
+
     // call otp service method.
-    return true;
+
+    return result;
   }
 }
