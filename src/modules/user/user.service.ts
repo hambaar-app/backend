32fb2ae1346038a4bcb25d9fbb0e3b7646c9f89c
Mohammad Hosseini <mimhe1381@gmail.com>
2025-07-22 20:12:59 +0330

    Add getTransporter to user service and Some improvements

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,24 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Prisma, User } from 'generated/prisma';
+import { Prisma, Transporter, User } from 'generated/prisma';
 import { UpdateTransporterDto } from './dto/update-transporter.dto';
 import { PrismaTransaction } from '../prisma/prisma.types';
+import { formatPrismaError } from 'src/common/utilities';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async get(userWhereUniqueInput: Prisma.UserWhereUniqueInput): Promise<User | null> {
-    return this.prisma.user.findUnique({
+  async get(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+    userInclude: Prisma.UserInclude = { transporter: true },
+    tx: PrismaService | PrismaTransaction = this.prisma
+  ): Promise<User | null> {
+    return tx.user.findUnique({
       where: userWhereUniqueInput,
-      include: {
-        transporter: true
-      }
+      include: userInclude
     });
   }
 
   async getByPhoneNumber(phoneNumber: string): Promise<User | null> {
     return this.get({ phoneNumber });
+  }
+
+  async getTransporter(
+    transporterWhereUniqueInput: Prisma.TransporterWhereUniqueInput,
+    transporterInclude?: Prisma.TransporterInclude,
+    tx: PrismaService | PrismaTransaction = this.prisma
+  ): Promise<Transporter | null> {
+    return tx.transporter.findUniqueOrThrow({
+      where: transporterWhereUniqueInput,
+      include: transporterInclude
+    }).catch((error: Error) => {
+      formatPrismaError(error);
+      throw error;
+    });
   }
 
   async updateTransporter(
@@ -31,6 +48,9 @@ export class UserService {
         userId
       },
       data: transporterDto
-    });
+    }).catch((error: Error) => {
+      formatPrismaError(error);
+      throw error;
+    });;
   }
 }
