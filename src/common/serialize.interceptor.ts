d502cf763c1eab9a6abde2dca9861b0a813fe6e7
Mohammad Hosseini <mimhe1381@gmail.com>
2025-07-26 11:25:14 +0330

    Add Serialize interceptor

---
+++ src/common/serialize.interceptor.ts
@@ -0,0 +1,34 @@
+import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+export type ClassConstructor<T> = new (...args: any[]) => T;
+
+export function Serialize<T>(dto: ClassConstructor<T>) {
+  return UseInterceptors(new SerializeInterceptor<T>(dto));
+}
+
+export class SerializeInterceptor<T> implements NestInterceptor {
+  constructor(private readonly dto: ClassConstructor<T>) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data: T | T[]) => {
+        if (data === null || data === undefined) {
+          return data;
+        }
+
+        const isArray = Array.isArray(data);
+        const dataToSerialize = isArray ? data : [data];
+
+        const result = plainToInstance(this.dto, dataToSerialize, {
+          excludeExtraneousValues: true,
+          enableImplicitConversion: true,
+          exposeDefaultValues: true,
+        });
+
+        return isArray ? result : result[0];
+      })
+    );
+  }
+}
