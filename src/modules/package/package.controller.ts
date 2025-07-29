1f89076a67da5d31f1743770aed2fbc9fa080401
Mohammad Hosseini <mimhe1381@gmail.com>
2025-07-29 13:57:07 +0330

    Add POST /packages

--- src/modules/package/package.controller.ts
+++ src/modules/package/package.controller.ts
@@ -6,6 +6,8 @@ import { AccessTokenGuard } from '../auth/guard/token.guard';
 import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
 import { RecipientResponseDto } from './dto/recipient-response.dto';
 import { Serialize } from 'src/common/serialize.interceptor';
+import { CreatePackageDto } from './dto/create-package.dto';
+import { PackageResponseDto } from './dto/package-response.dto';
 
 @Controller('package')
 export class PackageController {
@@ -30,5 +32,27 @@ export class PackageController {
   ) {
     const userId = req.user?.id;
     return this.packageService.createRecipient(userId!, body);
+  }
+
+  @ApiOperation({
+    summary: 'Create a new package',
+    description: `This endpoint creates a new package. The \`originAddressId\` must be a valid address Id
+      created via \`POST /addresses\` or obtained from the old addresses retrieved via \`GET /addresses\`.
+      Recipients can be included by providing recipient Ids created via \`POST /packages/recipients\`
+      or retrieved from \`GET /packages/recipients\`.`
+  })
+  @ApiCreatedResponse({
+    type: PackageResponseDto
+  })
+  @Serialize(PackageResponseDto)
+  @UseGuards(AccessTokenGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  async createPackage(
+    @Body() body: CreatePackageDto,
+    @Req() req: Request
+  ) {
+    const userId = req.user?.id;
+    return this.packageService.create(userId!, body);
   }
 }
--- src/modules/package/package.service.ts
+++ src/modules/package/package.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateRecipientDto } from './dto/create-recipient.dto';
+import { CreatePackageDto } from './dto/create-package.dto';
+import { instanceToPlain } from 'class-transformer';
+import { AuthMessages } from 'src/common/enums/messages.enum';
 
 @Injectable()
 export class PackageService {
@@ -8,7 +11,7 @@ export class PackageService {
     private prisma: PrismaService
   ) {}
 
-  createRecipient(
+  async createRecipient(
     userId: string,
     {
       address, ...recipientDto
@@ -27,6 +30,57 @@ export class PackageService {
       include: {
         address: true
       }
+    });
+  }
+
+  async create(
+    userId: string,
+    {
+      items,
+      originAddressId,
+      recipientId,
+      ...packageDto
+    }: CreatePackageDto
+  ) {
+    // const plainItems = instanceToPlain(items);
+    return this.prisma.$transaction(async tx => {
+      const originAddress = await tx.address.findFirst({
+        where: {
+          id: originAddressId,
+          userId,
+        }
+      });
+
+      if (!originAddress) {
+        throw new ForbiddenException(`${AuthMessages.EntityAccessDenied} origin address.`);
+      }
+
+      const recipient = await tx.packageRecipient.findFirst({
+        where: {
+          id: recipientId,
+          address: {
+            userId
+          }
+        }
+      });
+
+      if (!recipient) {
+        throw new ForbiddenException(`${AuthMessages.EntityAccessDenied} recipient.`);
+      }
+
+      return tx.package.create({
+        data: {
+          senderId: userId,
+          items,
+          originAddressId: originAddress.id,
+          recipientId: recipient.id,
+          ...packageDto
+        },
+        include: {
+          originAddress: true,
+          recipient: true
+        }
+      });
     });
   }
 }
