fdb904745170b601900e950940ce4db8524f5e8c
Mohammad Hosseini <mimhe1381@gmail.com>
2025-08-02 00:52:24 +0330

    Add calculateDistance service and its types

---
+++ src/modules/map/map.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CalculateDistanceInput, CalculateDistanceResult, DistanceMatrixResponse } from './map.types';
+import { ConfigService } from '@nestjs/config';
+import { TripTypeEnum } from 'generated/prisma';
+
+@Injectable()
+export class MapService {
+  private mapApiUrl: string;
+  private mapApiKey: string;
+
+  constructor(config: ConfigService) {
+    this.mapApiKey = config.getOrThrow<string>('MAP_API_KEY');
+    this.mapApiUrl = config.getOrThrow<string>('MAP_API_URL');
+  }
+
+  async calculateDistance(
+    {
+      vehicleType,
+      tripType,
+      origin,
+      destination
+    }: CalculateDistanceInput
+  ): Promise<CalculateDistanceResult> {
+    const url = this.mapApiUrl + '/distance-matrix'
+      + (tripType === TripTypeEnum.intercity ? '/no-traffic' : '')
+      + '?type=' + vehicleType
+      + '&origins=' + origin.latitude + ',' + origin.longitude
+      + '&destinations=' + destination.latitude + ',' + destination.longitude;
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Api-Key': this.mapApiKey,
+      },
+    });
+
+    if (!response.ok) {
+      throw new InternalServerErrorException();      
+    }
+
+    const data: DistanceMatrixResponse = await response.json();
+    return {
+      duration: data.rows[0].elements[0].duration.value / 60,
+      distance: data.rows[0].elements[0].distance.value / 1000
+    };
+  }
+}
