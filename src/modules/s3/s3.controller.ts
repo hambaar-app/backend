f4daf77e18c89c6185078afb197bd63fa2177d9f
Mohammad Hosseini <mimhe1381@gmail.com>
2025-08-06 10:46:04 +0330

    Add ownership guard to s3 and Complete responses docs

--- src/modules/s3/s3.controller.ts
+++ src/modules/s3/s3.controller.ts
@@ -4,6 +4,9 @@ import { FileNameDto } from './s3.dto';
 import { AccessTokenGuard } from '../auth/guard/token.guard';
 import { ApiOperation } from '@nestjs/swagger';
 import { CurrentUser } from '../user/current-user.middleware';
+import { CheckOwnership } from '../auth/auth.decorators';
+import { OwnershipGuard } from '../auth/guard/ownership.guard';
+import { AuthResponses } from 'src/common/api-docs.decorators';
 
 // TODO: Add limit
 @Controller('s3')
@@ -13,6 +16,7 @@ export class S3Controller {
   @ApiOperation({
     summary: 'Generate presigned URL for transporter profile picture upload',
   })
+  @AuthResponses()
   @UseGuards(AccessTokenGuard)
   @Get('/presigned/transporter/profile-pic')
   async getPresignedTransporterProfilePic(
@@ -27,6 +31,7 @@ export class S3Controller {
   @ApiOperation({
     summary: 'Generate presigned URL for transporter national id document upload',
   })
+  @AuthResponses()
   @UseGuards(AccessTokenGuard)
   @Get('/presigned/transporter/national-id')
   async getPresignedTransporterNationalId(
@@ -41,6 +46,7 @@ export class S3Controller {
   @ApiOperation({
     summary: 'Generate presigned URL for transporter driver’s license document upload',
   })
+  @AuthResponses()
   @UseGuards(AccessTokenGuard)
   @Get('/presigned/transporter/license')
   async getPresignedTransporterLicense(
@@ -55,7 +61,11 @@ export class S3Controller {
   @ApiOperation({
     summary: 'Generate presigned URL for vehicle picture upload',
   })
-  @UseGuards(AccessTokenGuard)
+  @AuthResponses()
+  @UseGuards(AccessTokenGuard, OwnershipGuard)
+  @CheckOwnership({
+    entity: 'vehicle'
+  })
   @Get('/presigned/transporter/vehicle/:id/pic')
   async getPresignedVehiclePicture(
     @Param('id', ParseUUIDPipe) vehicleId: string,
@@ -70,7 +80,11 @@ export class S3Controller {
   @ApiOperation({
     summary: 'Generate presigned URL for vehicle green sheet upload',
   })
-  @UseGuards(AccessTokenGuard)
+  @AuthResponses()
+  @UseGuards(AccessTokenGuard, OwnershipGuard)
+  @CheckOwnership({
+    entity: 'vehicle'
+  })
   @Get('/presigned/transporter/vehicle/:id/green-sheet')
   async getPresignedVehicleGreenSheet(
     @Param('id', ParseUUIDPipe) vehicleId: string,
@@ -85,7 +99,11 @@ export class S3Controller {
   @ApiOperation({
     summary: 'Generate presigned URL for vehicle card document upload',
   })
-  @UseGuards(AccessTokenGuard)
+  @AuthResponses()
+  @UseGuards(AccessTokenGuard, OwnershipGuard)
+  @CheckOwnership({
+    entity: 'vehicle'
+  })
   @Get('/presigned/transporter/vehicle/:id/card')
   async getPresignedVehicleCard(
     @Param('id', ParseUUIDPipe) vehicleId: string,
@@ -100,7 +118,11 @@ export class S3Controller {
   @ApiOperation({
     summary: 'Generate presigned URL for package picture upload',
   })
-  @UseGuards(AccessTokenGuard)
+  @AuthResponses()
+  @UseGuards(AccessTokenGuard, OwnershipGuard)
+  @CheckOwnership({
+    entity: 'package'
+  })
   @Get('/presigned/sender/package/:id')
   async getPresignedPackagePicture(
     @Param('id', ParseUUIDPipe) packageId: string,
