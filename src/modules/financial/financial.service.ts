cc91e1070329467504ffb2520409fc268c5aa153
Mohammad Hosseini <mimhe1381@gmail.com>
2025-08-20 19:28:41 +0330

    Add getWallet service

--- src/modules/financial/financial.service.ts
+++ src/modules/financial/financial.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { formatPrismaError } from 'src/common/utilities';
 
 @Injectable()
 export class FinancialService {
   constructor(private prisma: PrismaService) {}
+
+  async getWallet(userId: string) {
+    return this.prisma.wallet.findUniqueOrThrow({
+      where: { userId }
+    }).catch((error: Error) => {
+      formatPrismaError(error);
+      throw error;
+    });
+  }
 
   async addFunds(
     userId: string,
@@ -17,6 +27,9 @@ export class FinancialService {
           increment: amount
         }
       }
+    }).catch((error: Error) => {
+      formatPrismaError(error);
+      throw error;
     });
   }
 }
