86dbee5c930750497da6c7b5969896e308a0730a
Mohammad Hosseini <mimhe1381@gmail.com>
2025-08-26 18:20:12 +0330

    Add sendSms service

--- src/modules/sms/sms.service.ts
+++ src/modules/sms/sms.service.ts
@@ -6,16 +6,49 @@ import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class SmsService {
-  private otpApiKey: string;
+  private smsApiKey: string;
 
   constructor(
     private httpService: HttpService,
     config: ConfigService,
   ) {
-    this.otpApiKey = config.getOrThrow<string>('OTP_API_KEY');
+    this.smsApiKey = config.getOrThrow<string>('SMS_API_KEY');
+  }
+
+  async sendSms(mobiles: string[], message: string) {
+    try {
+      const url = 'https://s.api.ir/api/sw1/SendSms';
+
+      const response: AxiosResponse<{ success: boolean }> = await firstValueFrom(
+        this.httpService.post(
+          url,
+          {
+            mobiles,
+            message
+          }, 
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${this.smsApiKey}`,
+            }
+          }
+        ),
+      );
+
+      return response.data.success;
+    } catch (error) {
+      console.error('Failed to send sms.', error);      
+      throw new InternalServerErrorException('Failed to send sms.');
+    }
   }
 
   async sendOtp(mobile: string, code: number) {
+//     const otpMessage = `
+// کد تأیید هم‌بار: ${code}
+// این کد برای ورود به حساب کاربری شماست. لطفاً آن را با کسی به اشتراک نگذارید.
+// `;
+//     return this.sendSms([mobile], otpMessage);
+
     try {
       const url = 'https://s.api.ir/api/sw1/SmsOTP';
 
@@ -30,7 +63,7 @@ export class SmsService {
           {
             headers: {
               'Content-Type': 'application/json',
-              'Authorization': `Bearer ${this.otpApiKey}`,
+              'Authorization': `Bearer ${this.smsApiKey}`,
             }
           }
         ),
