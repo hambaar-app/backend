346d02bcd36413e33f86d3188c55f5abeac0682f
Mohammad Hosseini <mimhe1381@gmail.com>
2025-09-05 19:25:54 +0330

    Fix dashboard addressing

--- src/modules/dashboard/dashboard.service.ts
+++ src/modules/dashboard/dashboard.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { formatPrismaError, getDateDifference } from '../../common/utilities';
-import { PackageStatusEnum, PaymentStatusEnum, RequestStatusEnum, RolesEnum, TransactionTypeEnum, TripStatusEnum } from '../../../generated/prisma';
+import {
+  PackageStatusEnum,
+  PaymentStatusEnum,
+  RequestStatusEnum,
+  RolesEnum,
+  TransactionTypeEnum,
+  TripStatusEnum,
+} from '../../../generated/prisma';
 import { S3Service } from '../s3/s3.service';
 import { SenderStatistics, TransporterStatistics } from './dashboard.types';
 
@@ -9,43 +16,41 @@ import { SenderStatistics, TransporterStatistics } from './dashboard.types';
 export class DashboardService {
   constructor(
     private prisma: PrismaService,
-    private s3Service: S3Service
+    private s3Service: S3Service,
   ) {}
 
   async getDashboard(userId: string) {
-    const {
-      transporter,
-      wallet,
-      role,
-      ...user
-    } = await this.prisma.user.findFirstOrThrow({
-      where: { id: userId },
-      select: {
-        firstName: true,
-        lastName: true,
-        wallet: {
-          select: {
-            balance: true,
-            escrowedAmount: true
-          }
+    const { transporter, wallet, role, ...user } = await this.prisma.user
+      .findFirstOrThrow({
+        where: { id: userId },
+        select: {
+          firstName: true,
+          lastName: true,
+          wallet: {
+            select: {
+              balance: true,
+              escrowedAmount: true,
+            },
+          },
+          transporter: {
+            select: {
+              profilePictureKey: true,
+              rate: true,
+              bio: true,
+              firstTripDate: true,
+              lastTripDate: true,
+            },
+          },
+          role: true,
         },
-        transporter: {
-          select: {
-            profilePictureKey: true,
-            rate: true,
-            bio: true,
-            firstTripDate: true,
-            lastTripDate: true,
-          }
-        },
-        role: true
-      }
-    }).catch((error: Error) => {
-      formatPrismaError(error);
-      throw error;
-    });
+      })
+      .catch((error: Error) => {
+        formatPrismaError(error);
+        throw error;
+      });
 
-    const totalWalletBalance = (wallet?.balance ?? BigInt(0)) + BigInt(wallet?.escrowedAmount ?? 0);
+    const totalWalletBalance =
+      (wallet?.balance ?? BigInt(0)) + BigInt(wallet?.escrowedAmount ?? 0);
 
     let experience: string | undefined;
     if (
@@ -54,7 +59,10 @@ export class DashboardService {
       transporter?.lastTripDate &&
       transporter.lastTripDate > transporter.firstTripDate
     ) {
-      experience = getDateDifference(transporter.firstTripDate, transporter.lastTripDate);
+      experience = getDateDifference(
+        transporter.firstTripDate,
+        transporter.lastTripDate,
+      );
     }
 
     let statistics: TransporterStatistics | SenderStatistics | undefined;
@@ -69,38 +77,39 @@ export class DashboardService {
       fullName: `${user.firstName} ${user.lastName}`,
       totalBalance: totalWalletBalance,
       role,
-      profilePictureUrl: await this.s3Service.generateGetPresignedUrl(transporter?.profilePictureKey),
+      profilePictureUrl: await this.s3Service.generateGetPresignedUrl(
+        transporter?.profilePictureKey,
+      ),
       rate: transporter?.rate,
       experience,
       bio: transporter?.bio,
-      statistics
+      statistics,
     };
   }
 
-  private async getTransporterStatistics(userId: string): Promise<TransporterStatistics> {
+  private async getTransporterStatistics(
+    userId: string,
+  ): Promise<TransporterStatistics> {
     const completedTrips = await this.prisma.trip.count({
       where: {
         transporter: {
-          userId
-        }
-      }
+          userId,
+        },
+      },
     });
 
     const pendingRequests = await this.prisma.tripRequest.count({
       where: {
         trip: {
           transporter: {
-            userId
+            userId,
           },
           status: {
-            in: [
-              TripStatusEnum.scheduled,
-              TripStatusEnum.delayed
-            ]
+            in: [TripStatusEnum.scheduled, TripStatusEnum.delayed],
           },
         },
-        status: RequestStatusEnum.pending
-      }
+        status: RequestStatusEnum.pending,
+      },
     });
 
     const notDeliveredPackages = await this.prisma.package.count({
@@ -108,50 +117,51 @@ export class DashboardService {
         matchedRequest: {
           trip: {
             transporter: {
-              userId
-            }
-          }
+              userId,
+            },
+          },
         },
         status: {
           in: [
             PackageStatusEnum.matched,
             PackageStatusEnum.in_transit,
             PackageStatusEnum.picked_up,
-          ]
-        }
-      }
+          ],
+        },
+      },
     });
 
-    const { _sum: { amount: totalEscrowedAmount } } = await this.prisma.transaction.aggregate({
+    const {
+      _sum: { amount: totalEscrowedAmount },
+    } = await this.prisma.transaction.aggregate({
       where: {
         wallet: {
-          userId
+          userId,
         },
         transactionType: TransactionTypeEnum.escrow,
         matchedRequestId: {
-          notIn: (await this.prisma.transaction.findMany({
-            where: {
-              wallet: {
-                userId
+          notIn: (
+            await this.prisma.transaction.findMany({
+              where: {
+                wallet: {
+                  userId,
+                },
+                transactionType: {
+                  in: [TransactionTypeEnum.release, TransactionTypeEnum.refund],
+                },
               },
-              transactionType: {
-                in: [
-                  TransactionTypeEnum.release,
-                  TransactionTypeEnum.refund
-                ]
-              }
-            },
-            select: {
-              matchedRequestId: true
-            }
-          }))
-          .map(t => t.matchedRequestId)
-          .filter(i => i !== null)
-        }
+              select: {
+                matchedRequestId: true,
+              },
+            })
+          )
+            .map((t) => t.matchedRequestId)
+            .filter((i) => i !== null),
+        },
       },
       _sum: {
-        amount: true
-      }
+        amount: true,
+      },
     });
 
     return {
@@ -166,46 +176,45 @@ export class DashboardService {
     const notPickedUpPackages = await this.prisma.package.count({
       where: {
         senderId: userId,
-        status: PackageStatusEnum.matched
-      }
+        status: PackageStatusEnum.matched,
+      },
     });
 
     const inTransitPackages = await this.prisma.package.count({
       where: {
         senderId: userId,
         status: {
-          in: [
-            PackageStatusEnum.picked_up,
-            PackageStatusEnum.in_transit,
-          ]
-        }
-      }
+          in: [PackageStatusEnum.picked_up, PackageStatusEnum.in_transit],
+        },
+      },
     });
 
     const deliveredPackages = await this.prisma.package.count({
       where: {
         senderId: userId,
-        status: PackageStatusEnum.delivered
-      }
+        status: PackageStatusEnum.delivered,
+      },
     });
 
-    const { _sum: { finalPrice: totalUnpaidAmount } } = await this.prisma.package.aggregate({
+    const {
+      _sum: { finalPrice: totalUnpaidAmount },
+    } = await this.prisma.package.aggregate({
       where: {
         senderId: userId,
         matchedRequest: {
           paymentStatus: PaymentStatusEnum.unpaid,
-        }
+        },
       },
       _sum: {
-        finalPrice: true
-      }
+        finalPrice: true,
+      },
     });
 
     return {
       notPickedUpPackages,
       inTransitPackages,
       deliveredPackages,
-      totalUnpaidAmount: totalUnpaidAmount ?? 0
+      totalUnpaidAmount: totalUnpaidAmount ?? 0,
     };
   }
 }
