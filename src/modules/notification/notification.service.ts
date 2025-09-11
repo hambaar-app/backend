0c2a94f6e5f5e01fa2a8494c56f7e7a4be3581e9
Mohammad Hosseini <mimhe1381@gmail.com>
2025-09-11 11:35:14 +0330

    Add create notification service

--- src/modules/notification/notification.service.ts
+++ src/modules/notification/notification.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(userId: string, content: string) {
+    return this.prisma.notification.create({
+      data: {
+        userId,
+        content
+      }
+    });
+  }
+}
